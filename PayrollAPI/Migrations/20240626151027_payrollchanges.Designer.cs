6e33f04731356d9e7ee17a51ca925b2b77600682
sologia <solorzanogian1@gmail.com>
2024-06-26 09:11:51 -0600

    I create a new migration, because I realized changes on payroll.cs

--- PayrollAPI/Migrations/20240626151027_payrollchanges.Designer.cs
+++ PayrollAPI/Migrations/20240626151027_payrollchanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PayrollAPI.Data;
 
@@ -11,9 +12,11 @@ using PayrollAPI.Data;
 namespace PayrolAPI.Migrations
 {
     [DbContext(typeof(PayrollContext))]
-    partial class PayrollContextModelSnapshot : ModelSnapshot
+    [Migration("20240626151027_payrollchanges")]
+    partial class payrollchanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PayrollAPI/Migrations/20240626151027_payrollchanges.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PayrolAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class payrollchanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "FechaPeriodo",
+                table: "Payrolls",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Periodo",
+                table: "Payrolls",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaPeriodo",
+                table: "Payrolls");
+
+            migrationBuilder.DropColumn(
+                name: "Periodo",
+                table: "Payrolls");
+        }
+    }
+}
